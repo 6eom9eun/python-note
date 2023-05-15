a3d8a32c76ed082d713b9ffb56f02c662f9f9be8
6eom9eun <104510730+6eom9eun@users.noreply.github.com>
2023-05-15 14:21:00 +0900

    Update 1439-뒤집기.py

--- Baekjoon/그리디/1439-뒤집기.py
+++ Baekjoon/그리디/1439-뒤집기.py
@@ -3,10 +3,10 @@ S = input()
 count = 0
 
 for i in range(len(S)-1):
-    if i != S[i+1]:
+    if S[i] != S[i+1]:
         count += 1
 
-print((count+1)//2) 
+print((count+1)//2)
 
 # 0 or 1로 변환하면 되니까, 2로 나눈 몫.
 # 0 -> 0 -> 0
