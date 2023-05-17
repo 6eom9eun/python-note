66a5bba361199adba6900a9aac49dd378dc7d2db
6eom9eun <104510730+6eom9eun@users.noreply.github.com>
2023-05-17 17:24:45 +0900

    Update 5-8_DFS 예제.py

--- Icote/5-8_DFS 예제.py
+++ Icote/5-8_DFS 예제.py
@@ -1,23 +1,23 @@
 def DFS(graph, v, visited):
-    visited[v] = True
+    visited[v] = True       #현재 노드 방문 처리
     print(v, end='')
 
-    for i in graph[v]:
+    for i in graph[v]:      #현재 노드와 연결된 노드를 재귀적으로 방문
         if not visited[i]:
             DFS(graph, i, visited)
 
 graph = [
     [],
-    [2, 3, 8],
-    [1, 7],
+    [2, 3, 8],      #1번 노드 인접 노드
+    [1, 7],     #2번 노드 인접 노드 ...
     [1, 4, 5],
     [3, 5],
     [3, 4],
     [7],
     [2, 6, 8],
-    [1, 7]
+    [1, 7],     #... 8번 노드 인접 노드
 ]
 
-visited = [False] * 9
+visited = [False] * 9       #각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
 
-DFS(graph, 1, visited)
+DFS(graph, 1, visited)      #정의된 DFS 함수 호출
